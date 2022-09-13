a90f0ca3e3fdca8e0621b9734ad97fd36e13eecb
mildata <milda.tamasauskaite@gmail.com>
2022-09-13 08:30:03 +0200

    initial routing (#4)

--- src/components/Content.js
+++ src/components/Content.js
@@ -1,3 +1,4 @@
+import { Route, Routes } from "react-router";
 import BreedPicturesPage from "../pages/BreedPicturesPage";
 import BreedsListPage from "../pages/BreedsListPage";
 import FavoritePicturesPage from "../pages/FavoritePicturesPage";
@@ -9,21 +10,34 @@ import FavoritePictures from "./favorite-pictures/FavoritePictures";
 const Content = (props) => {
   return (
     <>
-      <div>
-        <h1>Content</h1>
-        
-        <BreedsListPage>
-          <BreedsList />
-        </BreedsListPage>
-
-        <BreedPicturesPage>
-          <BreedPictures />
-        </BreedPicturesPage>
-
-        <FavoritePicturesPage>
-          <FavoritePictures />
-        </FavoritePicturesPage>
-      </div>
+      <h1>Content</h1>
+      <Routes>
+        <Route
+          path="/"
+          element={
+            <BreedsListPage>
+              <BreedsList />
+            </BreedsListPage>
+          }
+        />
+        <Route
+          path="/breed/:breed"
+          element={
+            <BreedPicturesPage>
+              <BreedPictures />
+            </BreedPicturesPage>
+          }
+        />
+        <Route
+          path="/favorite-pictures"
+          element={
+            <FavoritePicturesPage>
+              <FavoritePictures />
+            </FavoritePicturesPage>
+          }
+        />
+        <Route path="*"/>
+      </Routes>
     </>
   );
 };
