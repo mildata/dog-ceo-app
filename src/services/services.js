07dbaeb726446d16f70a96026d628f088672c91b
mildata <milda.tamasauskaite@gmail.com>
2022-09-20 09:56:18 +0200

    error handling changed in service

--- src/services/services.js
+++ src/services/services.js
@@ -1,20 +1,28 @@
 import { URLS } from "../models/constants";
 
 export async function fetchAllBreeds() {
-  const response = await fetch(URLS.allBreeds);
-  if (!response.ok) {
-    const message = `An error has occured in fetchAllBreeds service: ${response.status}`;
-    throw new Error(message);
+  try {
+    const response = await fetch(URLS.allBreeds);
+    if (!response.ok) {
+      throw new Error(`HTTP error: ${response.status}`);
+    }
+    const data = await response.json();
+    return data;
+  } catch (error) {
+    console.error(`An error has occured in fetchAllBreeds service: ${error}`);
   }
-  return await response.json();
 }
 
 export async function fetchBreedImages(breedName) {
   const url = URLS.breedImages.replace("name", breedName);
-  const response = await fetch(url);
-  if (!response.ok) {
-    const message = `An error has occured in fetchBreedImage service: ${response.status}`;
-    throw new Error(message);
+  try {
+    const response = await fetch(url);
+    if (!response.ok) {
+      throw new Error(`HTTP error: ${response.status}`);
+    }
+    const data = await response.json();
+    return data;
+  } catch (error) {
+    console.error(`An error has occured in fetchBreedImages service: ${error}`);
   }
-  return await response.json();
 }
